0810bd861b37f40de7793ed38cd9bfbbcd28f49c
Tai Phan <taip2704@gmail.com>
2025-08-17 17:21:07 +0700

    Fix: Import dj_database_url in settings

--- khupho_project/settings.py
+++ khupho_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os # <-- THÊM DÒNG NÀY ĐỂ IMPORT THƯ VIỆN 'os'
+import dj_database_url # <-- THÊM DÒNG NÀY VÀO
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
