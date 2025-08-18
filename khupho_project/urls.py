3ac5b6f42734e79997d4183d60fec423e577a0cd
Tai Phan <taip2704@gmail.com>
2025-08-18 10:52:46 +0700

    Add homepage redirect to announcements page

--- khupho_project/urls.py
+++ khupho_project/urls.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
-from django.urls import path, include # 1. Sửa dòng này để thêm 'include'
+from django.urls import path, include
+from django.views.generic.base import RedirectView # 1. Thêm import này
+# Thêm 2 dòng import này để xử lý file media
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
+    # 2. Thêm dòng này: Chuyển hướng trang chủ đến trang /thong-bao/
+    path('', RedirectView.as_view(url='/thong-bao/', permanent=True)),
+
     path('admin/', admin.site.urls),
-    path('thong-bao/', include('thongbao.urls')), # 2. Thêm dòng này
-    path('danh-ba/', include('dancu.urls')), # Thêm dòng này
+    path('thong-bao/', include('thongbao.urls')),
+    path('danh-ba/', include('dancu.urls')),
     path('gop-y/', include('phananh.urls')),
-    path('bieu-mau/', include('bieumau.urls')), # Thêm dòng này
-    path('bao-cao/', include('baocao.urls')), # Thêm dòng này
+    path('bieu-mau/', include('bieumau.urls')),
+    path('bao-cao/', include('baocao.urls')),
     path('tai-khoan/', include('django.contrib.auth.urls')),
 ]
-# Thêm dòng này vào cuối file để server có thể hiển thị ảnh bạn đã upload
+
+# Dòng này giữ nguyên
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
